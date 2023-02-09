45830d9061d90490bd49ca05ba795529017fd88a
Mykola <107625914+kolya-kyb@users.noreply.github.com>
2023-02-09 11:50:16 +0200

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,50 @@ import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Message from './Notification/Notification';
 
-import { Component } from 'react';
+import { useState } from 'react';
 
+export const App = () => {
+  const [statistic, setStatistic] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const buttons = Object.keys(statistic);
+
+  const handleClick = txt => {
+    setStatistic(prevState => ({ ...prevState, [txt]: prevState[txt] + 1 }));
+  };
+
+  const countTotalFeedback = Object.values(statistic).reduce(
+    (acc, value) => acc + value,
+    0
+  );
+
+  const positivePercentage = Math.floor(
+    (statistic.good * 100) / countTotalFeedback
+  );
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={buttons} onLeaveFeedback={handleClick} />
+      </Section>
+
+      <Section title="Statistic">
+        {countTotalFeedback ? (
+          <Statistics
+            good={statistic.good}
+            neutral={statistic.neutral}
+            bad={statistic.bad}
+            countTotalFeedback={countTotalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Message message="There is no feedback" />
+        )}
+      </Section>
+    </>
+  );
+};
+
+/*
 export class App extends Component {
   state = {
     good: 0,
@@ -52,3 +94,4 @@ export class App extends Component {
     );
   }
 }
+*/
